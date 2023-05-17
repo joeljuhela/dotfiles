71e99f9d7bd15219d39fc14558a8fb81812fc058
Joel Juhela <joel.juhela@protonmail.com>
2023-05-17 16:42:28 +0300

    set noshowmode

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,19 +1,20 @@
 
 vim.opt.clipboard = 'unnamedplus' 	-- use system clipboard
-vim.opt.mouse = 'a'			-- allow mouse
+vim.opt.mouse = 'a'		    	-- allow mouse
 
 -- Tab
-vim.opt.tabstop = 4			-- visual tab size
+vim.opt.tabstop = 4			    -- visual tab size
 vim.opt.softtabstop = 4			-- tab size in editing
 vim.opt.shiftwidth = 4			-- insert 4 spaces with tab
 vim.opt.expandtab = true		-- tabs are spaces
 
 -- UI
 vim.opt.number = true			-- show linenumbers
-vim.opt.relativenumber = true		-- relative numbering
+vim.opt.relativenumber = true	-- relative numbering
 vim.opt.cursorline = true		-- highlight the line where cursor is
 vim.opt.splitbelow = true		-- open vertical splits from bottom
 vim.opt.splitright = true		-- open horizontal splits from right
+vim.cmd('set noshowmode')       -- remove --INSERT-- from below lualine
 
 -- Search
 vim.opt.incsearch = true		-- search as characters are entered
