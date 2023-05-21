c21dedfedf5a31510aea48fa191a4cf2dff0d765
Joel Juhela <joel.juhela@protonmail.com>
2023-05-21 19:29:45 +0300

    Add vim as a global variable in lua_ls config

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -16,9 +16,22 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Kudos for Sampo Rapeli, this is taken from his dotfiles, check: https://github.com/samporapeli/dotfiles
 local lspconfig = require('lspconfig')
 for _, server_name in ipairs(require('mason-lspconfig').get_installed_servers()) do
-    lspconfig[server_name].setup({
-        on_attach = lsp_attach,
-        capabilities = capabilities
-    })
+    if server_name == 'lua_ls' then
+        lspconfig[server_name].setup({
+            on_attach = lsp_attach,
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' } -- add vim as a global variable
+                    }
+                }
+            }
+        })
+    else
+        lspconfig[server_name].setup({
+            on_attach = lsp_attach,
+            capabilities = capabilities
+        })
+    end
 end
-
