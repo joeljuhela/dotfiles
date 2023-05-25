f773c3d09188563f71e61ab03004db023f51d7d4
Joel Juhela <joel.juhela@protonmail.com>
2023-05-25 21:58:17 +0300

    Better keybinds for completion

--- .config/nvim/lua/core/plugin_config/completion.lua
+++ .config/nvim/lua/core/plugin_config/completion.lua
@@ -5,7 +5,7 @@ cmp.setup({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-o>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
+        ['<TAB>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true })
     }),
     snippet = {
