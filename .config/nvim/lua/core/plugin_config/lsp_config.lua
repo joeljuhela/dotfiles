a6a80738948830bd33394dbeac26850aa1a0c20e
Joel Juhela <joel.juhela@protonmail.com>
2023-05-29 15:16:33 +0300

    Add LSP keybinds

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -8,7 +8,11 @@ require('mason-lspconfig').setup({
 
 local lsp_attach = function(_, _)
     -- custom keybinds
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { desc = { "Go to definition" } })
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, { desc = { "Hover" } })
+    vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float)
+    vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
