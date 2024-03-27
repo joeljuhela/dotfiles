cf661edee0df8a0bc47018a6fd73686f8c8070f9
Joel Juhela <joel.juhela@protonmail.com>
2024-03-27 20:09:28 +0200

    LSP setup

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,6 +2,18 @@ local lsp = require("lsp-zero")
 
 lsp.preset('recommended')
 
+-- Fix Undefined global 'vim'
+lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+
 local cmp = require('cmp')
 local cmp_action = lsp.cmp_action()
 
@@ -9,6 +21,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp_action.luasnip_jump_backward(),
     ['<C-n>'] = cmp_action.luasnip_jump_forward(),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete()
   })
 })
@@ -18,6 +31,12 @@ lsp.on_attach(function(_, bufnr)
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 
@@ -28,4 +47,8 @@ require('mason-lspconfig').setup({
   handlers = {
     lsp.default_setup,
   },
+})
+
+vim.diagnostic.config({
+  virtual_text = false
 })
